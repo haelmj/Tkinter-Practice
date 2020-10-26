5928acd882940cbc47413934d726f0d2c8d9e52d
Michael Ndon <manj19.mjn@gmail.com>
2020-10-26 14:21:31 +0100

    Created buttons

--- Bookshop_Database/script.py
+++ Bookshop_Database/script.py
@@ -1,3 +1,15 @@
+"""
+A program that stores book information:
+Title, Author, Year, ISBN
+
+User can: 
+-View all books
+-Search an entry
+-Add an entry
+-Update entry
+-Delete
+-Close
+"""
 from tkinter import *
 
 window = Tk()
@@ -32,4 +44,22 @@ e4_value=StringVar()
 e4 = Entry(window, textvariable=e4_value)
 e4.grid(row=1, column=3)
 
+#create buttons
+b1 = Button(window, text='View all')
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text='Search entry')
+b2.grid(row=3, column=3)
+
+b3 = Button(window, text='Add entry')
+b3.grid(row=4, column=3)
+
+b4 = Button(window, text='Update')
+b4.grid(row=5, column=3)
+
+b5 = Button(window, text='Delete')
+b5.grid(row=6, column=3)
+
+b6 = Button(window, text='Close')
+b6.grid(row=7, column=3)
 window.mainloop()
