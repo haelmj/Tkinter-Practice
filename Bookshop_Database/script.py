e8d98bfafbb46e0a4884fd8d6162be644cab8a24
Michael Ndon <manj19.mjn@gmail.com>
2020-10-26 14:07:06 +0100

    Modified entries to have text variable

--- Bookshop_Database/script.py
+++ Bookshop_Database/script.py
@@ -16,16 +16,20 @@ l4 = Label(window, text='ISBN')
 l4.grid(row=1, column=2)
 
 # create entries
-e1 = Entry(window)
+e1_value=StringVar()
+e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
-e2 = Entry(window)
+e2_value=StringVar()
+e2 = Entry(window, textvariable=e2_value)
 e2.grid(row=1, column=1)
 
-e3 = Entry(window)
+e3_value=StringVar()
+e3 = Entry(window, textvariable=e3_value)
 e3.grid(row=0, column=3)
 
-e4 = Entry(window)
+e4_value=StringVar()
+e4 = Entry(window, textvariable=e4_value)
 e4.grid(row=1, column=3)
 
 window.mainloop()
