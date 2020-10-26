b61928f0107092b75a4434dd4e2dcd8dae74886f
Michael Ndon <manj19.mjn@gmail.com>
2020-10-26 14:04:43 +0100

    Created Labels and Entry fields

---
+++ Bookshop_Database/script.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+window = Tk()
+# create labels at the top
+
+l1 = Label(window, text='Title')
+l1.grid(row=0, column=0)
+
+l2 = Label(window, text='Year')
+l2.grid(row=1, column=0)
+
+l3 = Label(window, text='Author')
+l3.grid(row=0, column=2)
+
+l4 = Label(window, text='ISBN')
+l4.grid(row=1, column=2)
+
+# create entries
+e1 = Entry(window)
+e1.grid(row=0, column=1)
+
+e2 = Entry(window)
+e2.grid(row=1, column=1)
+
+e3 = Entry(window)
+e3.grid(row=0, column=3)
+
+e4 = Entry(window)
+e4.grid(row=1, column=3)
+
+window.mainloop()
