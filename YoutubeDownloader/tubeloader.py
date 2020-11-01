5aec122b061d9de6b8533956c8f87446600accea
Michael Ndon <manj19.mjn@gmail.com>
2020-11-01 22:10:50 +0100

    Added functionality to browse location button

--- YoutubeDownloader/tubeloader.py
+++ YoutubeDownloader/tubeloader.py
@@ -4,8 +4,9 @@ Simple GUI YouTube Downloader
 """
 
 from pytube import YouTube
-from tkinter import *
 from tkinter import ttk
+from tkinter import *
+from tkinter.filedialog import askdirectory
 import os
 
 # get default download folder path
@@ -50,7 +51,19 @@ def download_video():
     if file_path.get():
         yt.streams.first().download(file_path.get())
     else:
+        entry2.delete(0, END)
+        entry2.insert(0, DEFAULT_PATH)
         yt.streams.first().download(DEFAULT_PATH)
+
+def browse_folder():
+    try:
+        folder_path = askdirectory()
+        entry2.delete(0, END)
+        entry2.insert(0,folder_path)
+    except Exception:
+        entry2.delete(0, END)
+        entry2.insert(0, DEFAULT_PATH)
+
 
 style = ttk.Style(window)
 style.configure("Placeholder.TEntry", foreground="#d5d5d5")
@@ -69,7 +82,7 @@ entry2.grid(row=5, column=2, columnspan=4)
 b2 = Button(window, text='Download', width=12, command= download_video)
 b2.grid(row=2, column=6, pady=5)
 
-b1 = Button(window, text='Browse Folder')
+b1 = Button(window, text='Browse Folder', command=browse_folder)
 b1.grid(row=5, column=6)
 
 window.mainloop()
