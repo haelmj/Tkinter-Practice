74debe08f6b9aaf31f5d613907a90d20da9932fb
Michael Ndon <manj19.mjn@gmail.com>
2020-11-01 21:56:28 +0100

    Added functionality to accept file path instead of default

--- YoutubeDownloader/tubeloader.py
+++ YoutubeDownloader/tubeloader.py
@@ -1,23 +1,30 @@
+"""
+Simple GUI YouTube Downloader
+
+"""
+
 from pytube import YouTube
 from tkinter import *
 from tkinter import ttk
 import os
 
+# get default download folder path
 DEFAULT_PATH = os.path.join( os.getenv('USERPROFILE'), 'Downloads')
 
 window = Tk()
-window.wm_title('YouTube Downloader') # set up the title and size.
-window.geometry('500x200')  # set up the size
+# set up the title and size
+window.wm_title('YouTube Downloader') 
+window.geometry('400x170')  
 window.resizable(width=True, height=True)
 
-top = Frame(window, width=500, height=50)
-top.pack(side=TOP)
-bottom = Frame(window, width=500, height=50)
-bottom.pack(side=BOTTOM)
-left = Frame(window,  width=300, height=100)
-left.pack(side=LEFT)
-right = Frame(window, width=50, height=100)
-right.pack(side=RIGHT)
+# top = Frame(window, width=500, height=50)
+# top.pack(side=TOP)
+# bottom = Frame(window, width=500, height=50)
+# bottom.pack(side=BOTTOM)
+# left = Frame(window,  width=300, height=100)
+# left.pack(side=LEFT)
+# right = Frame(window, width=50, height=100)
+# right.pack(side=RIGHT)
 
 class PlaceholderEntry(ttk.Entry):
     def __init__(self, container, placeholder, *args, **kwargs):
@@ -40,23 +47,29 @@ class PlaceholderEntry(ttk.Entry):
 
 def download_video():
     yt = YouTube(yt_link.get())
-    yt.streams.first().download(DEFAULT_PATH)
+    if file_path.get():
+        yt.streams.first().download(file_path.get())
+    else:
+        yt.streams.first().download(DEFAULT_PATH)
 
 style = ttk.Style(window)
 style.configure("Placeholder.TEntry", foreground="#d5d5d5")
 
 yt_link = StringVar()
-entry = PlaceholderEntry(left, "URL", textvariable=yt_link, width=50)
-entry.grid(row=2, column=2, columnspan=4)
+entry = PlaceholderEntry(window, "Youtube Link", textvariable=yt_link, width=50)
+entry.grid(row=2, column=2, columnspan=4, padx=5, pady=5)
+
+l1 = Label(window, text='----------')
+l1.grid(row=3, column =2, columnspan=4)
 
 file_path = StringVar()
-entry2 = PlaceholderEntry(left, "Download Path", textvariable=file_path, width=50)
+entry2 = PlaceholderEntry(window, "Destination Path", textvariable=file_path, width=50)
 entry2.grid(row=5, column=2, columnspan=4)
 
-b2 = Button(right, text='Download', width=12, command= download_video)
-b2.grid(row=2, column=6)
+b2 = Button(window, text='Download', width=12, command= download_video)
+b2.grid(row=2, column=6, pady=5)
 
-b1 = Button(right, text='Choose Download Location')
+b1 = Button(window, text='Browse Folder')
 b1.grid(row=5, column=6)
 
 window.mainloop()
